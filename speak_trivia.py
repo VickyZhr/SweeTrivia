e0d9e2b17bb6dcd0824448fc5fa959b8a33e16c3
Min Ji Kim <minjiki2@andrew.cmu.edu>
2025-02-24 11:47:29 -0500

    micropython version of speak_trivia.py

--- speak_trivia.py
+++ speak_trivia.py
@@ -1,16 +1,17 @@
-import os
-import json
-import random
+import ujson
+import urandom
+from machine import Pin
 
 # Load questions from the JSON file
 with open('questions.json', 'r') as file:
-    trivia_questions = json.load(file)
+    trivia_questions = ujson.load(file)
 
 def narrate_question(question):
-    os.system(f'espeak-ng -v en+m3 -s 60 "{question}"')
+    print(f"Narrating: {question}")
+    # Here, instead of os.system/espeak-ng, you can trigger an external module via serial or sound output
 
 # Pick a random question
-question = random.choice(trivia_questions)["question"].strip()
+question = urandom.choice(trivia_questions)["question"].strip()
 
 # Speak the question
 narrate_question(question)
