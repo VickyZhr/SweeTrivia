be7a7ca5f98a609423c8746aa760b4e8234c8d4a
Min Ji Kim <minjiki2@andrew.cmu.edu>
2025-03-08 21:57:56 -0500

    made the audio a little bit slower

--- speak_trivia.py
+++ speak_trivia.py
@@ -4,7 +4,7 @@ import random
 
 # Initialize pyttsx3 engine
 engine = pyttsx3.init()
-engine.setProperty('rate', 180)
+engine.setProperty('rate', 150)
 engine.setProperty('volume', 1.0) # Max volume
 
 # Set voice (change index if needed)
