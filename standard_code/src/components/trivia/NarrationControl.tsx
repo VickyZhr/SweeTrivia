334353e8270e0e05c5bdfcc6cb2bb2adb05855b3
VickyZhr <91871586+VickyZhr@users.noreply.github.com>
2025-04-10 20:18:21 -0400

    repeated calls to speak() avoid

--- standard_code/src/components/trivia/NarrationControl.tsx
+++ standard_code/src/components/trivia/NarrationControl.tsx
@@ -1,13 +1,3 @@
-
-// import React from 'react';
-
-// // This component is now empty as narration functionality has been removed
-// const NarrationControl: React.FC = () => {
-//   return null;
-// };
-
-// export default NarrationControl;
-
 import React, { useEffect } from 'react';
 import { speakWithEspeakNg } from '@/utils/speechUtils';
 
@@ -23,14 +13,22 @@ const NarrationControl: React.FC<NarrationControlProps> = ({
   onNarrationEnd,
 }) => {
   useEffect(() => {
+    let isCancelled = false;
+
     const narrate = async () => {
       const narration = `Question: ${question}. Option A: ${options[0]}. Option B: ${options[1]}. Option C: ${options[2]}. Option D: ${options[3]}.`;
       await speakWithEspeakNg(narration);
-      onNarrationEnd();
+      if (!isCancelled) {
+        onNarrationEnd();
+      }
     };
 
     narrate();
-  }, [question, options, onNarrationEnd]);
+
+    return () => {
+      isCancelled = true;
+    };
+  }, [question]);
 
   return null;
 };
